f539e7ad9ea060fd11a13c9341302a8e976657ee
Lettie123 <65544306+Lettie123@users.noreply.github.com>
2020-11-16 22:19:28 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+var val = document.getElementById("valR").value;
+document.getElementById("img").src = ".jpg";
+  function showVal(newVal){
+     if(newVal >=90 && newVal < 100)
+    document.getElementById("img").src = "img/a01.png";
+     else if(newVal >= 80 && newVal < 90)
+    document.getElementById("img").src = "img/a02.png";
+     else if(newVal >= 70 && newVal < 80)
+    document.getElementById("img").src = "img/a03.png";
+     else if(newVal >= 60 && newVal < 70)
+    document.getElementById("img").src = "img/a04.png";
+     else if(newVal >= 50 && newVal < 60)
+    document.getElementById("img").src = "img/a05.png";
+     else if(newVal >= 40 && newVal < 50)
+    document.getElementById("img").src = "img/a06.png";
+     else if(newVal >= 30 && newVal < 40)
+    document.getElementById("img").src = "img/a07.png";
+     else if(newVal >= 20 && newVal < 30)
+    document.getElementById("img").src = "img/a08.png";
+    else if(newVal >= 10 && newVal < 20)
+    document.getElementById("img").src = "img/a09.png";
+}
+
+var val = document.getElementById("valR2").value;
+document.getElementById("img").src = ".jpg";
+  function showVal2(newVal){
+     if(newVal >0 && newVal < 10)
+    document.getElementById("img").src = "img/b01.png";
+     else if(newVal >= 10 && newVal < 20)
+    document.getElementById("img").src = "img/b02.png";
+    else if(newVal >= 20 && newVal < 30)
+    document.getElementById("img").src = "img/b03.png";
+    else if(newVal >= 30 && newVal < 40)
+    document.getElementById("img").src = "img/b04.png";
+    else if(newVal >= 40 && newVal < 50)
+    document.getElementById("img").src = "img/b05.png";
+    else if(newVal >= 50 && newVal < 60)
+    document.getElementById("img").src = "img/b06.png";
+}
